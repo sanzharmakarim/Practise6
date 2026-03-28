848f84d7e9a2b3ac1738b97869127b01a680291c
sanzhar <ma_nurzhanuly@kbtu.kz>
2026-03-28 12:27:03 +0500

    Update tasks.py

--- File handling/tasks.py
+++ File handling/tasks.py
@@ -1,24 +1,24 @@
 import shutil
 import os
 
-#task1
+
 with open("text.txt", "w") as f:
    f.write("Hello, my name is Maulenali!")
 
 with open("text.txt") as f:
     print(f.read())
 
-#task2
+
 with open("text.txt", "r") as f:
     print(f.read())
 
-#task3
+
 with open("text.txt", "a") as f:
     f.write(" My hobby is playing video games. ")
 with open("text.txt") as f:
     print(f.read())
 
-#task4
+
 shutil.copy("text.txt", "text_2.txt")
 with open("text_2.txt", "a", encoding="utf-8") as f:
     f.write(" And i like reading books!")
